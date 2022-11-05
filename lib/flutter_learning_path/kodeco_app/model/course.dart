f679c7c8eee8c8d86f49e41fc8704082cfecd130
Akash Singh <goandsatish@gmail.com>
2022-11-05 13:43:11 +0530

    added more fields to course model class

--- lib/flutter_learning_path/kodeco_app/model/course.dart
+++ lib/flutter_learning_path/kodeco_app/model/course.dart
@@ -1,56 +1,28 @@
-import 'dart:convert';
-
 class Course {
   final String id;
   final String name;
   final String description;
-
+  final String? difficulty;
+  final String? contributor;
+  final String artworkUrl;
   Course({
+    required this.artworkUrl,
     required this.id,
     required this.name,
     required this.description,
+    this.difficulty,
+    this.contributor,
   });
-
-  Course copyWith({
-    String? id,
-    String? name,
-    String? description,
-  }) {
-    return Course(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      description: description ?? this.description,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'name': name,
-      'description': description,
-    };
-  }
-
-  String toJson() => json.encode(toMap());
 
   Course.fromJson(Map<String, dynamic> json)
       : id = json['id'] as String,
         name = json['attributes']['name'] as String,
-        description = json['attributes']['description_plain_text'] as String;
+        description = json['attributes']['description_plain_text'] as String,
+        artworkUrl = json['attributes']['card_artwork_url'] as String,
+        difficulty = json['attributes']['difficulty'] as String?,
+        contributor = json['attributes']['contributor_string'] as String?;
 
   @override
   String toString() =>
-      'Course(id: $id, name: $name, description: $description)';
-
-  @override
-  bool operator ==(covariant Course other) {
-    if (identical(this, other)) return true;
-
-    return other.id == id &&
-        other.name == name &&
-        other.description == description;
-  }
-
-  @override
-  int get hashCode => id.hashCode ^ name.hashCode ^ description.hashCode;
+      'Course(id: $id, name: $name, description: $description, difficulty: $difficulty, contributor: $contributor, artworkUrl: $artworkUrl)';
 }
