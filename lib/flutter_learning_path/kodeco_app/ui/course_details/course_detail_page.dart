8b11f371faf895d1bd410631a8dcddd5d0aa0682
Akash Singh <goandsatish@gmail.com>
2022-11-05 17:42:00 +0530

    adding more content to course-details-page.

--- lib/flutter_learning_path/kodeco_app/ui/course_details/course_detail_page.dart
+++ lib/flutter_learning_path/kodeco_app/ui/course_details/course_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'image_container.dart';
 import '../../model/course.dart';
+import '../../utils/string_extension.dart';
 
 class CourseDetailPage extends StatelessWidget {
   final Course course;
@@ -14,7 +16,66 @@ class CourseDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(course.name),
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _buildBanner(),
+          _buildMain(context),
+          _buildDetails(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBanner() => ImageContainer(
+        height: 300,
+        url: course.artworkUrl,
+      );
+
+  Widget _buildMain(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            course.name,
+            style:
+                Theme.of(context).textTheme.subtitle2?.copyWith(fontSize: 24),
+          ),
+          Text(
+            course.description,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetails(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            "Domains: ${course.domainString}",
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          Text(
+            "Level: ${course.difficulty?.capitalize()}",
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          Text(
+            "${course.contributor}",
+            style: const TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
